da1736aa5f1b642d0e64cbcd82dda934e7a85c4e
Alberto Pulido Gómez <apulido@atsistemas.com>
2021-07-19 11:28:48 +0200

    Show images

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use(express.static(__dirname+'/public'));
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
