07ccf410579a69e18b145f7a75e3b5e835b8ec29
Alberto Pulido Gómez <apulido@atsistemas.com>
2021-07-22 12:19:03 +0200

    Refactorizando código

--- router/mascotas.js
+++ router/mascotas.js
@@ -2,12 +2,28 @@ const express = require('express');
 const multer = require('multer');
 const mimeTypes = require('mime-types');
 const router = express.Router();
+const Ajv = require("ajv");
+
+const ajv = new Ajv(); // Validador
 
 const Mascota = require('../models/mascota'); //Modelo
 
-router.get('/', async(req, res) => {
+const schema = {
+    type: "object",
+    properties: {
+        nombre: { type: "string" },
+        descripcion: { type: "string" },
+        imagen: { type: "string" },
+    },
+    required: ["nombre", "descripcion"],
+    additionalProperties: false
+}
+
+const validate = ajv.compile(schema);
+
+// OBTENER TODAS LAS MASCOTAS
+router.get('/', async (req, res) => {
     try {
-        
         const arrayMascotasDB = await Mascota.find()
         res.render("mascotas", {
             arrayMascotas: arrayMascotasDB
@@ -15,7 +31,7 @@ router.get('/', async(req, res) => {
 
     } catch (error) {
         console.log(error)
-    }    
+    }
 })
 
 // CREAR NUEVA MASCOTA
@@ -24,34 +40,32 @@ router.get('/crear', (req, res) => {
 });
 const storage = multer.diskStorage({
     destination: 'public/uploads/',
-    filename: function(req,file,cb){
+    filename: function (req, file, cb) {
         cb("", Date.now() + '.' + mimeTypes.extension(file.mimetype))
     }
 });
 const upload = multer({
     storage: storage
 });
-router.post('/', upload.single('imagen') , async (req, res) => {
-    req.body = {...req.body, 'imagen': req.file.filename}
+router.post('/', upload.single('imagen'), async (req, res) => {
+    req.body = { ...req.body, 'imagen': req.file.filename }
     const body = req.body;
-    try {
-        // const mascotaDB = new Mascota(body);
-        // await mascotaDB.save();
-
-        await Mascota.create(body);
-
-        res.redirect('/mascotas')
-    } catch (error) {
-        console.log('error', error);
+    const valid = validate(body);
+    if (!valid) console.log(validate.errors);
+    else {
+        await Mascota.create(body)
+            .then(() => res.redirect('/mascotas'))
+            .catch(err => console.log('error', err));
     }
 })
 
-router.get('/:id', async(req, res) => {
+// ACTUALIZAR Y ELIMIANAR
+router.get('/:id', async (req, res) => {
 
     const _id = req.params.id;
 
     try {
-        const mascotaDB = await Mascota.findOne({_id: _id});
+        const mascotaDB = await Mascota.findOne({ _id: _id });
 
         res.render('detalle', {
             mascota: mascotaDB,
@@ -64,45 +78,37 @@ router.get('/:id', async(req, res) => {
         })
     }
 })
-
-router.delete('/:id', async(req,res) => {
+router.delete('/:id', async (req, res) => {
     const _id = req.params.id;
 
-    try {
-        const mascotaDB = await Mascota.findByIdAndDelete({_id: _id});
-
-        if (mascotaDB) {
-            res.json({
-                estado: true,
-                mensaje: 'Eliminado'
-            })
-        }else{
+    await Mascota.findByIdAndDelete({ _id: _id })
+        .then(() => res.json({
+            estado: true,
+            mensaje: 'Eliminado'
+        }))
+        .catch(err => {
             res.json({
                 estado: false,
                 mensaje: 'Fallo eliminar'
             })
-        }
-    } catch (error) {
-        console.log(error);
-    }
+            console.log(err)
+        });
 })
-
-router.put('/:id', async(req, res) => {
+router.put('/:id', async (req, res) => {
     const _id = req.params.id;
     const body = req.body;
-
-    try {
-        const mascotaDB = await Mascota.findByIdAndUpdate(_id, body, { useFindAndModify: false});
-
-        res.json({
-            estado: true,
-            mensaje: "Editado"
-        })
-    } catch (error) {
-        res.json({
-            estado: false,
-            mensaje: "No Editado"
-        })
+    const valid = validate(body);
+    if (!valid) console.log(validate.errors);
+    else {
+        await Mascota.findByIdAndUpdate(_id, body, { useFindAndModify: false })
+            .then(() => res.json({
+                estado: true,
+                mensaje: "Editado"
+            }))
+            .catch(() => res.json({
+                estado: false,
+                mensaje: "No Editado"
+            }));
     }
 })
 
